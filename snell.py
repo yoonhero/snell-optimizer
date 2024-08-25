042f25d2019bd0fcc29c1832159fb9c3cd874433
yoonhero <yoonhero06@naver.com>
2024-08-25 10:50:49 +0900

    .

--- snell.py
+++ snell.py
@@ -22,7 +22,7 @@ class SnellOptimizer:
                         new_dir = torch.stack(vs)
                         param -= self.learning_rate * new_dir
                     else:
-                        # If is there any previous record, just step 
+                        # If there isn't any previous record, just step 
                         param -= self.learning_rate * param.grad
                     self.prev[i] = copy.deepcopy(grad)
 
@@ -32,26 +32,23 @@ class SnellOptimizer:
                 return cur
             if torch.all(cur==prev) or (cur-prev).count_nonzero() <= 1:
                 return cur
-            norm_cur, norm_prev = torch.norm(cur)+self.eps, torch.norm(prev)+self.eps
-            cos = torch.dot(cur, prev) / (norm_cur * norm_prev + self.eps)
-            a = (1-cos**2).clip(0, 1)
-            sin1 = torch.sqrt(a)
-            cross = torch.cross(cur, prev)
+            norm_cur = torch.norm(cur).clamp(min=self.eps)
+            norm_prev = torch.norm(prev).clamp(min=self.eps)
+            cos = torch.clamp(torch.dot(cur, prev) / (norm_cur * norm_prev), min=-1, max=1)
+            sin1 = torch.sqrt((1-cos**2).clamp(min=0, max=1))
+            cross = torch.cross(cur, prev, dim=0)
             vel_cur, vel_prev = torch.exp(-norm_cur), torch.exp(-norm_prev)
 
             u_cur = cur / norm_cur
-            u_cross = cross / torch.norm(cross)
+            cross_norm = torch.norm(cross).clamp(min=self.eps)
+            u_cross = cross / cross_norm
             u_ortho = torch.cross(u_cur, u_cross)
-            # new basis for x, y, z coord
             basis = torch.stack([u_cur, u_ortho, u_cross], dim=1)
 
-            # snell's law
-            sin2 = sin1 / (vel_prev+self.eps) * vel_cur
-            # prevent reflecting all
-            sin2 = sin2.clip(0, 1)
-            cos2 = torch.sqrt(1-sin2**2).clip(0, 1)
+            sin2 = torch.clamp(sin1 * vel_cur / vel_prev, max=1)
+            cos2 = torch.sqrt(1 - sin2**2)
 
-            new_dir = torch.tensor([cos2, sin2, 0])
+            new_dir = torch.stack([cos2, sin2, torch.tensor(0.0)])
             return basis @ new_dir * norm_cur
 
     def zero_grad(self):
